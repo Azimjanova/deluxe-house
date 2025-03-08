084e4a6f2cafbd73dd0db428257c634dff879dce
elmira <esenalievaelmira011@gmail.com>
2025-03-08 19:35:57 +0600

    upDate

---
+++ src/pages/home/HomePage.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const PageHeader = () => {
+    return (
+        <div>
+            <div>
+                <p>DELUXE HOUSE
+                    design studio</p>
+            </div>
+            <div>
+                <a href="">Услуги</a>
+                <a href="">Ремонт</a>
+                <a href="">Портфолио</a>
+                <a href="">О нас</a>
+                <a href="">Статьи</a>
+                <a href="">Контакты</a>
+            </div>
+            <div>
+                <img src="" alt=""/>
+                <p>8 (700) 300-03-40</p>
+            </div>
+        </div>
+    );
+};
+
+export default PageHeader;
